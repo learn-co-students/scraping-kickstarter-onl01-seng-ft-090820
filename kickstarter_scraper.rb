8594765a7fc8d803442acc509ea8fb0475785204
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-29 22:01:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,11 @@
 # require libraries/modules here
+requre 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
